63e810923bc23b459be77d8ec352bcd8c8f45f10
Dagestan05 <steklofan@gmail.com>
2018-07-20 16:10:43 +0200

    added NewsSlider Component

---
+++ src/components/widgets/NewsSlider/sliderTemplates.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SliderTemplates = ({news}) => {
+  console.log(news)
+  return ( 
+    <div>
+      SliderTemplates
+    </div>
+   );
+}
+ 
+export default SliderTemplates;
